ed8a6a9bdfe8bf513e79eef08ed07436223647d9
Mihan Maneth <mihanmaneth2019@gmail.com>
2026-04-02 14:42:26 +0530

    Unversioned files

---
+++ src/main/java/com/sliit/studentplatform/module3/dto/response/TaskSummaryResponse.java
@@ -0,0 +1,17 @@
+package com.sliit.studentplatform.module3.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Map;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskSummaryResponse {
+    private Map<String, Long> statusCounts;
+    private long totalTasks;
+}
